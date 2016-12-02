97ad0b6f5684e5ebe9bb390e8d3e1a8e306d47eb
6348 <rchtt1996@gmail.com>
2016-12-02 10:27:11 +0700

    GGWP

---
+++ OOP.py
@@ -0,0 +1,30 @@
+import arcade
+ 
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+ 
+class SpaceGameWindow(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+ 
+        arcade.set_background_color(arcade.color.BLACK)
+ 
+        self.ship = arcade.Sprite('images/ship.png')
+        self.ship.set_position(100, 100)
+
+    def animate(self, delta):
+        ship = self.ship
+ 
+        if ship.center_y > SCREEN_HEIGHT:
+            ship.center_y = 0
+        ship.set_position(ship.center_x, ship.center_y + 5)
+
+    def on_draw(self):
+        arcade.start_render()
+ 
+        self.ship.draw()
+ 
+ 
+if __name__ == '__main__':
+    window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.run()
